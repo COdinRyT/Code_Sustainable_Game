21b73019fce05ce0fea0b4e06b6be48eafa483e2
LostPhantom18 <45826132+LostPhantom18@users.noreply.github.com>
2025-01-24 14:15:18 -0500

    Added settings and changed sample scene name

--- CodeSustainableGame/Assets/Scripts/Menu/GoToMenu.cs
+++ CodeSustainableGame/Assets/Scripts/Menu/GoToMenu.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GoToGame : MonoBehaviour
+public class GoToMenu : MonoBehaviour
 {
     public void OnClick()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Menu");
     }
 }
