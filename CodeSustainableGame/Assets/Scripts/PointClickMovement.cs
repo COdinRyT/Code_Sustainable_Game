d508977c2925919159f92bc54c6e3f58a5ad1dc9
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2025-01-26 15:46:50 -0500

    Created clickable movement script

--- CodeSustainableGame/Assets/Scripts/PointClickMovement.cs
+++ CodeSustainableGame/Assets/Scripts/PointClickMovement.cs
@@ -1,20 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Scripting.APIUpdating;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public float playerSpeed = 1.0f;
+    [SerializeField]
+    private InputAction mouseClick;
 
-    // Start is called before the first frame update
-    void Start()
+    private Camera camera;
+    private Coroutine coroutine;
+
+    public float playerSpeed = 10f;
+
+    private Vector3 targetPosition;
+
+
+    private void Awake()
     {
-        
+        camera = Camera.main;
+    }
+    private void OnEnable()
+    {
+        mouseClick.Enable();
+        mouseClick.performed += Move;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        mouseClick.performed -= Move;
+        mouseClick.Disable();
     }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        Ray ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if(Physics.Raycast(ray: ray, hitInfo: out RaycastHit hit) && hit.collider)
+        {
+            if(coroutine != null) StopCoroutine(coroutine);            
+            coroutine = StartCoroutine(PlayerMoveTowards(hit.point));
+            targetPosition = hit.point;
+        }
+    }
+
+    private IEnumerator PlayerMoveTowards(Vector3 target)
+    {
+        while(Vector2.Distance(transform.position, target) > 0.1f)
+        {
+            Vector3 destination = Vector3.MoveTowards(transform.position, target, playerSpeed * Time.deltaTime);
+            transform.position = destination;
+            yield return null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(targetPosition, 1);
+    }
+
+
 }
