fd567bc8e55bacba8835a0cbc384bcb5e702ea47
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2025-02-27 23:57:22 -0500

    Select and movement implemented

--- CodeSustainableGame/Assets/Scripts/PointClickMovement.cs
+++ CodeSustainableGame/Assets/Scripts/PointClickMovement.cs
@@ -23,7 +23,12 @@ public class NewBehaviourScript : MonoBehaviour
     private Rigidbody rb;
     [SerializeField] private GameObject[] gridCoordinates; // Array of valid tiles
     private GameObject selectedPlayer = null;  // The character that the player selects
+    private GameObject selectedTile = null; // The tile that the player selects
+    //Booleans to keep track on whether player or tile have been selected
     public bool isPlayerSelected;
+    private bool isTileSelected;
+    //This is to keep track of how many times an object is selected
+    private int timesSelected;
 
     private Vector3 normalizePoint;
     private Vector3 midNormalizePoints;
@@ -36,6 +41,7 @@ public class NewBehaviourScript : MonoBehaviour
         camera = Camera.main;
         rb = GetComponent<Rigidbody>();
         isPlayerSelected = false;
+        isTileSelected = false;
 
         // Find all tiles dynamically if not assigned
         if (gridCoordinates.Length == 0)
@@ -55,152 +61,154 @@ public class NewBehaviourScript : MonoBehaviour
                 if (hit.collider.gameObject.layer == 6)
                 {
                     SelectPlayer(hit.collider.gameObject);
+                    timesSelected++;
+                    if(hit.collider.gameObject.layer == 6 && timesSelected ==2)
+                    {
+                        isPlayerSelected=false;
+                        timesSelected = 0;
+                        Debug.Log("Player unselected");
+                    }
                 }
-                if (hit.collider.gameObject.layer == 7 && isPlayerSelected == true);
+
+                if(hit.collider.gameObject.layer == 7 && isPlayerSelected == true)
                 {
-                    MoveSelectedPlayer(hit.point);                  
+                    SelectTile(hit.collider.gameObject);
                 }
-
-                //normalizePoint = hit.point;
-
-                ////Debug.Log(normalizePoint.x);
-                ////Debug.Log(Math.Truncate(normalizePoint.x));
-                ////xdecimalPoint = Mathf.Floor(normalizePoint.x) - normalizePoint.x;
-                ////Debug.Log(xdecimalPoint);
-                //// Whole number plus 0.5 
-
-                //xdecimalPoint = Mathf.Round(hit.point.x);
-                //zdecimalPoint = Mathf.Round(hit.point.z);
-                //midNormalizePoints = new Vector3(xdecimalPoint, hit.point.y, zdecimalPoint);
-                //Debug.Log(normalizePoint);
-                //Debug.Log(midNormalizePoints);
-                //agent.SetDestination(midNormalizePoints);
-                //Instantiate(prefab, midNormalizePoints, Quaternion.identity);
-                //Debug.Log(normalizePoint.x);
-                //Debug.Log(Math.Truncate(normalizePoint.x));
-                //xdecimalPoint = Mathf.Floor(normalizePoint.x) - normalizePoint.x;
-                //Debug.Log(xdecimalPoint);
-                // Whole number plus 0.5 
-                
-                xdecimalPoint = Mathf.Round(hit.point.x);
-                zdecimalPoint = Mathf.Round(hit.point.z);
-                midNormalizePoints = new Vector3(xdecimalPoint, 1, zdecimalPoint);
-                //Debug.Log(normalizePoint);
-                //Debug.Log(midNormalizePoints);
-                agent.SetDestination(midNormalizePoints);
-                Instantiate(prefab, midNormalizePoints, Quaternion.identity);
-                CheckIfOnGarbage.Instance.x = midNormalizePoints.x;
-                CheckIfOnGarbage.Instance.y = midNormalizePoints.y;
-                CheckIfOnGarbage.Instance.z = midNormalizePoints.z;
-                CheckIfOnGarbage.Instance.CheckCollisionBetweenPlayerAndGarbage();
             }
         }
     }
 
+    //When this function is called, make the player the selected game object 
     public void SelectPlayer(GameObject player)
     {
         selectedPlayer = player;
         isPlayerSelected = true;
-        Debug.Log("Player has been selected");
+        Debug.Log("Player has been Selected");
     }
 
-    public void MoveSelectedPlayer(Vector3 targetPoint)
+    public void SelectTile(GameObject tile)
     {
-        if(selectedPlayer != null)
-        {
-            normalizePoint = targetPoint;
+        selectedTile = tile;
+        isTileSelected = true;        
+        Instantiate(prefab, selectedTile.transform.position, Quaternion.identity) ;
+        Debug.Log("Tile selected");
+    }
 
-            //Debug.Log(normalizePoint.x);
-            //Debug.Log(Math.Truncate(normalizePoint.x));
-            //xdecimalPoint = Mathf.Floor(normalizePoint.x) - normalizePoint.x;
-            //Debug.Log(xdecimalPoint);
-            // Whole number plus 0.5 
+    //Move the player when this function is called
+    public void MovePlayer(Vector3 targetPoint)
+    {
 
-            xdecimalPoint = Mathf.Round(targetPoint.x);
-            zdecimalPoint = Mathf.Round(targetPoint.z);
-            midNormalizePoints = new Vector3(xdecimalPoint, targetPoint.y, zdecimalPoint);
-            Debug.Log(normalizePoint);
-            Debug.Log(midNormalizePoints);
-            agent.SetDestination(midNormalizePoints);
-            Instantiate(prefab, midNormalizePoints, Quaternion.identity);
-        }
-        else
-        {
-            Debug.Log("Player is not selected");
-        }
+        xdecimalPoint = Mathf.Round(targetPoint.x);
+        zdecimalPoint = Mathf.Round(targetPoint.z);
+        midNormalizePoints = new Vector3(xdecimalPoint, 1, zdecimalPoint);
+        //Debug.Log(normalizePoint);
+        //Debug.Log(midNormalizePoints);
+        agent.SetDestination(midNormalizePoints);
+        Instantiate(prefab, midNormalizePoints, Quaternion.identity);
+        CheckIfOnGarbage.Instance.x = midNormalizePoints.x;
+        CheckIfOnGarbage.Instance.y = midNormalizePoints.y;
+        CheckIfOnGarbage.Instance.z = midNormalizePoints.z;
+        CheckIfOnGarbage.Instance.CheckCollisionBetweenPlayerAndGarbage();
+    }
+
+    //public void SelectPlayer(GameObject player)
+    //{
+    //    selectedPlayer = player;
+    //    isPlayerSelected = true;
+    //    Debug.Log("Player has been selected");
+    //}
+
+    //public void MoveSelectedPlayer(Vector3 targetPoint)
+    //{
+    //    if(selectedPlayer != null)
+    //    {
+    //        normalizePoint = targetPoint;
+
+    //        //Debug.Log(normalizePoint.x);
+    //        //Debug.Log(Math.Truncate(normalizePoint.x));
+    //        //xdecimalPoint = Mathf.Floor(normalizePoint.x) - normalizePoint.x;
+    //        //Debug.Log(xdecimalPoint);
+    //        // Whole number plus 0.5 
+
+    //        xdecimalPoint = Mathf.Round(targetPoint.x);
+    //        zdecimalPoint = Mathf.Round(targetPoint.z);
+    //        midNormalizePoints = new Vector3(xdecimalPoint, targetPoint.y, zdecimalPoint);
+    //        Debug.Log(normalizePoint);
+    //        Debug.Log(midNormalizePoints);
+    //        agent.SetDestination(midNormalizePoints);
+    //        Instantiate(prefab, midNormalizePoints, Quaternion.identity);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Player is not selected");
+    //    }
        
-    }
+    //}
 
-    /*
-    private void OnEnable()
-    {
-        mouseClick.Enable();
-        mouseClick.performed += SelectTile;
-
-        confirmMove.Enable();
-        confirmMove.performed += ConfirmMovement;
-    }
-
-    //This function is called when the script is enabled
-    private void OnDisable()
-    {
-        mouseClick.performed -= SelectTile;
-        mouseClick.Disable();
-
-        confirmMove.performed -= ConfirmMovement;
-        confirmMove.Disable();
-    }
     
-    /// <summary>
-    /// Selects a grid tile when clicked.
-    /// </summary>
-    private void SelectTile(InputAction.CallbackContext context)
-    {
-        //This is to project a ray from the camera position onto wherever the mouse 
-        //is currently positioned
-        Ray ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            foreach (GameObject gridTile in gridCoordinates)
-            {
-                if (hit.collider.gameObject == gridTile)
-                {
-                    selectedTile = gridTile;
-                    Debug.Log("Selected Tile: " + selectedTile.name);
-                    return;
-                }
-            }
-        }
-    }
-    */
-    /*
-    /// <summary>
-    /// Confirms movement and moves player toward the selected tile step-by-step.
-    /// </summary>
-    private void ConfirmMovement(InputAction.CallbackContext context)
-    {
-        if (selectedTile != null)
-        {
-            if (moveCoroutine != null) StopCoroutine(moveCoroutine);
-            moveCoroutine = StartCoroutine(MoveStepByStep(selectedTile.transform.position));
-        }
-    }
+    //private void OnEnable()
+    //{
+    //    mouseClick.Enable();
+    //    mouseClick.performed += SelectTile;
 
-    /// <summary>
-    /// Moves the player step by step toward the target position.
-    /// </summary>
-    private IEnumerator MoveStepByStep(Vector3 target)
-    {
-        target.y = transform.position.y; // Keep the player at the same height
+    //    confirmMove.Enable();
+    //    confirmMove.performed += ConfirmMovement;
+    //}
 
-        while (Vector3.Distance(transform.position, target) > 0.1f)
-        {
-            Vector3 nextStep = Vector3.MoveTowards(transform.position, target, playerSpeed * Time.deltaTime);
-            transform.position = nextStep;
-            yield return new WaitForSeconds(stepDelay); // Adds a delay between steps for a turn-based feel
-        }
+    ////This function is called when the script is enabled
+    //private void OnDisable()
+    //{
+    //    mouseClick.performed -= SelectTile;
+    //    mouseClick.Disable();
 
-        transform.position = target; // Snap to the exact position
-    }
-    */
+    //    confirmMove.performed -= ConfirmMovement;
+    //    confirmMove.Disable();
+    //}
+    
+    
+    //private void SelectTile(InputAction.CallbackContext context)
+    //{
+    //    //This is to project a ray from the camera position onto wherever the mouse 
+    //    //is currently positioned
+    //    Ray ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+    //    if (Physics.Raycast(ray, out RaycastHit hit))
+    //    {
+    //        foreach (GameObject gridTile in gridCoordinates)
+    //        {
+    //            if (hit.collider.gameObject == gridTile)
+    //            {
+    //                selectedTile = gridTile;
+    //                Debug.Log("Selected Tile: " + selectedTile.name);
+    //                return;
+    //            }
+    //        }
+    //    }
+    //}
+    //*/
+    
+    
+    //private void ConfirmMovement(InputAction.CallbackContext context)
+    //{
+    //    if (selectedTile != null)
+    //    {
+    //        if (moveCoroutine != null) StopCoroutine(moveCoroutine);
+    //        moveCoroutine = StartCoroutine(MoveStepByStep(selectedTile.transform.position));
+    //    }
+    //}
+
+    
+    //private IEnumerator MoveStepByStep(Vector3 target)
+    //{
+    //    target.y = transform.position.y; // Keep the player at the same height
+
+    //    while (Vector3.Distance(transform.position, target) > 0.1f)
+    //    {
+    //        Vector3 nextStep = Vector3.MoveTowards(transform.position, target, playerSpeed * Time.deltaTime);
+    //        transform.position = nextStep;
+    //        yield return new WaitForSeconds(stepDelay); // Adds a delay between steps for a turn-based feel
+    //    }
+
+    //    transform.position = target; // Snap to the exact position
+    //}
+    
 }
