833e7d068a7f5c13b9e7b9da2e26b75bbe81cfe1
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2025-03-08 19:26:19 -0500

    Adding camera control into actual level

--- CodeSustainableGame/Assets/Scripts/NPCManager.cs
+++ CodeSustainableGame/Assets/Scripts/NPCManager.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class NPCManager : MonoBehaviour
 {
-    public Vector3 spawnPosition;
+    private Vector3 spawnPosition;
+    public float spawnCounter;
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.SetActive(false);
+        spawnPosition = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (CheckIfOnGarbage.Instance.PlayerAndGarbageCollision)
+        {
+            gameObject.SetActive(true);
+        }
     }
+
 }
