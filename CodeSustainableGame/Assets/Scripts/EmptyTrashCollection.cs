cf42095501023d7807411bb4f6d8d2ab9f428c03
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2025-03-20 19:32:47 -0400

    I hate sliders

--- CodeSustainableGame/Assets/Scripts/EmptyTrashCollection.cs
+++ CodeSustainableGame/Assets/Scripts/EmptyTrashCollection.cs
@@ -8,6 +8,7 @@ public class EmptyTrashCollection : MonoBehaviour
 {
 
     [SerializeField] private Slider progressSlider;
+    [SerializeField] private Image progressImage;
     [SerializeField] private int totalGarbage = 10;
     TruckAI truck;
     private int garbageDestroyed = 0;
