e1f0c85102c44528b76723bc21f204e4f57e643e
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2025-04-03 16:14:40 -0400

    Game can now pause

--- CodeSustainableGame/Assets/Scripts/Menu/PauseMenu.cs
+++ CodeSustainableGame/Assets/Scripts/Menu/PauseMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
@@ -10,18 +11,34 @@ public class PauseMenu : MonoBehaviour
 
     private void Update()
     {
+        
+    }
 
+    public void PauseClick() 
+    {
+        if (isPausedMenuOpen) 
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
     }
 
 
     private void Pause()
     {
-
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        isPausedMenuOpen=true;
     }
 
     private void Resume()
     {
-
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPausedMenuOpen=false;
     }
 
 
