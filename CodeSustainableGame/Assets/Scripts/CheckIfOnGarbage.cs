ebbb524370c8931c09a557e95f795988991f13d3
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2025-04-03 17:31:46 -0400

    Change garbage health

--- CodeSustainableGame/Assets/Scripts/CheckIfOnGarbage.cs
+++ CodeSustainableGame/Assets/Scripts/CheckIfOnGarbage.cs
@@ -40,7 +40,7 @@ public class CheckIfOnGarbage : MonoBehaviour
                 {
                     if (allChildren[i].GetComponent<Garbage>().currentHealth > 0)
                     {
-                        allChildren[i].GetComponent<Garbage>().currentHealth -= 50;
+                        allChildren[i].GetComponent<Garbage>().currentHealth -= 25;
                         allChildren[i].transform.localScale = currentScale * 0.8f;
                         Debug.Log(allChildren[i].GetComponent<Garbage>().currentHealth);
                         //Destroy(allChildren[i]);
